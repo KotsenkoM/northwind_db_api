35b707d7f96449b6d5ade8776b197c61636d7129
KotsenkoM <maksimkcenk93@gmail.com>
2023-03-22 11:10:49 +0300

    add db environ

--- src/database.py
+++ src/database.py
@@ -1,12 +1,12 @@
+import os
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost/northwind'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(os.getenv('DB_URL'))
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
