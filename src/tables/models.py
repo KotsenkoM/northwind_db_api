b6e2391f04e1874a936f04806532c56ee2a91f70
KotsenkoM <maksimkcenk93@gmail.com>
2023-03-22 18:18:46 +0300

    model refactor

--- src/tables/models.py
+++ src/tables/models.py
@@ -1,25 +1,25 @@
 from sqlalchemy import Column, Integer, String, Date, Float, ForeignKey
 from src.database import Base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 
 class Order(Base):
     __tablename__ = 'orders'
 
-    order_id = Column(Integer, primary_key=True)
-    customer_id = Column(String, primary_key=True)
-    employee_id = Column(Integer)
-    order_date = Column(Date)
-    required_date = Column(Date)
-    shipped_date = Column(Date)
-    ship_via = Column(Integer)
-    freight = Column(Float)
-    ship_name = Column(String)
-    ship_address = Column(String)
-    ship_city = Column(String)
-    ship_region = Column(String)
-    ship_postal_code = Column(String)
-    ship_country = Column(String)
+    order_id = Column('order_id', primary_key=True)
+    customer_id = Column('customer_id')
+    employee_id = Column('employee_id')
+    order_date = Column('order_date')
+    required_date = Column('required_date')
+    shipped_date = Column('shipped_date')
+    ship_via = Column('ship_via')
+    freight = Column('freight')
+    ship_name = Column('ship_name')
+    ship_address = Column('ship_address')
+    ship_city = Column('ship_city')
+    ship_region = Column('ship_region')
+    ship_postal_code = Column('ship_postal_code')
+    ship_country = Column('ship_country')
 
     # items = relationship('Customer', back_populates='owner')
 
